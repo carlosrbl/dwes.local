392978beb9f6ae90c42098778cf67e92c0109607
Carlos Rodrigo Beltrá <carrodbel3@alu.edu.gva.es>
2025-10-03 10:41:21 +0200

    second commit

--- templates/views/navegacion.part.php
+++ templates/views/navegacion.part.php
@@ -14,10 +14,53 @@
          </div>
          <div class="collapse navbar-collapse navbar-right" id="menu">
             <ul class="nav navbar-nav">
-              <li class="active lien"><a href="index.php"><i class="fa fa-home sr-icons"></i> Home</a></li>
-              <li class=" lien"><a href="about.php"><i class="fa fa-bookmark sr-icons"></i> About</a></li>
-              <li class=" lien"><a href="blog.php"><i class="fa fa-file-text sr-icons"></i> Blog</a></li>
-              <li><a href="contact.php"><i class="fa fa-phone-square sr-icons"></i> Contact</a></li>
+              <?php 
+                if (esOpcionMenuActiva('/index.php')==true || esOpcionMenuActiva('/')==true)
+                {
+                  echo '<li class="active lien">';
+                }
+                else
+                {
+                  echo '<li class="lien">';
+                } 
+              ?>
+              <a href="index.php"><i class="fa fa-home sr-icons"></i> Home</a></li>
+              
+              <?php 
+                if (esOpcionMenuActiva('/about.php')==true)
+                {
+                  echo '<li class="active lien">';
+                }
+                else
+                {
+                  echo '<li class="lien">';
+                } 
+              ?>
+              <a href="about.php"><i class="fa fa-bookmark sr-icons"></i> About</a></li>
+              
+              <?php 
+                if (esOpcionMenuActiva('/blog.php')==true)
+                {
+                  echo '<li class="active lien">';
+                }
+                else
+                {
+                  echo '<li class="lien">';
+                } 
+              ?>
+              <a href="blog.php"><i class="fa fa-file-text sr-icons"></i> Blog</a></li>
+              
+              <?php 
+                if (esOpcionMenuActiva('/contact.php')==true)
+                {
+                  echo '<li class="active lien">';
+                }
+                else
+                {
+                  echo '<li class="lien">';
+                } 
+              ?>
+              <a href="contact.php"><i class="fa fa-phone-square sr-icons"></i> Contact</a></li>
             </ul>
          </div>
      </div>
